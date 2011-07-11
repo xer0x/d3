2ded0da610cd82aff358dfed6d8ef7b482d74066
Mike Bostock <mbostock@gmail.com>
2011-07-10 21:50:12 -0700

    Add "minor" mode.
    This subdivides the ticks, showing both "major" and "minor" ticks.

--- src/chart/axis.js
+++ src/chart/axis.js
@@ -5,6 +5,7 @@ d3.chart.axis = function() {
       tickPadding = 3,
       mode = "open",
       open = true,
+      minor = false,
       section = false,
       scale;
 
@@ -23,6 +24,9 @@ d3.chart.axis = function() {
         range = [range[0], range[range.length - 1]];
       }
 
+      // Invert inverted domains.
+      if (domain[0] > domain[1]) domain = [domain[1], domain[0]];
+
       // In "closed" mode, if the tick value is also in the domain, then we
       // don't want to draw the line; it will be drawn by the domain path below.
       // We do, of course, still want to draw the label for the tick. Also, by
@@ -31,6 +35,17 @@ d3.chart.axis = function() {
       var display = open ? null : function(d) {
         return domain.indexOf(d) === -1 ? null : "none";
       };
+
+      // In "minor" mode, subdivide the generated ticks so that there are extra
+      // minor ticks between the major ones. This requires that the ticks are
+      // evenly spaced (so no log ticks, no polylinear ticks, etc.). Also,
+      // there's a chance that the first and last tick extend beyond the domain
+      // and we need to avoid displaying those!
+      if (minor) {
+        ticks = d3_chart_axisSubdivide(ticks);
+        var bit = ticks[0] < domain[0] - d3_chart_axisEpsilon ? (ticks.shift(), 1) : 0;
+        if (ticks[ticks.length - 1] > domain[1] + d3_chart_axisEpsilon) ticks.pop();
+      }
 
       // If a custom tick format is specified, used that; otherwise, use the
       // scale's tick format. Note this means that a tick format is required if
@@ -49,7 +64,9 @@ d3.chart.axis = function() {
       // Enter a container for incoming ticks.
       var tickEnter = tick.enter().append("svg:g")
           .attr("class", "tick")
-          .attr("transform", function(d) { return "translate(" + scale(d) + ",0)"; });
+          .attr("transform", function(d) { return "translate(" + scale(d) + ",0)"; })
+          .classed("minor", minor && function(d, i) { return (i & 1) === bit; })
+          .classed("major", minor && function(d, i) { return (i & 1) !== bit; });
 
       // Add the tick line.
       tickEnter.append("svg:line")
@@ -61,9 +78,9 @@ d3.chart.axis = function() {
       tickEnter.append("svg:text")
           .attr("text-anchor", "middle")
           .attr("y", (section ? 0 : Math.max(-tickSize, 0)) + tickPadding)
-          .attr("x", section ? function(d, i) { return i < ticks.length - 1 ? (scale(ticks[i + 1]) - scale(d)) / 2 : null; } : null)
+          .attr("x", section ? function(d, i) { return i < ticks.length - 1 - minor ? (scale(ticks[i + 1 + minor]) - scale(d)) / 2 : null; } : null)
           .attr("dy", ".71em")
-          .style("display", section ? function(d, i) { return i < ticks.length - 1 ? null : "none"; } : null)
+          .style("display", section ? function(d, i) { return (i < ticks.length - 1 - minor) && (!minor || ((i & 1) !== bit)) ? null : "none"; } : (minor ? function(d, i) { return (i & 1) !== bit ? null : "none"; } : null))
           .text(format);
 
       // Update the tick positions and visibility.
@@ -115,7 +132,8 @@ d3.chart.axis = function() {
   axis.mode = function(x) {
     if (!arguments.length) return mode;
     mode = "" + x;
-    open = /(?:^|-)open(?:-|$)/i.test(mode);
+    open = !/(?:^|-)closed(?:-|$)/i.test(mode);
+    minor = /(?:^|-)minor(?:-|$)/i.test(mode);
     section = /(?:^|-)section(?:-|$)/i.test(mode);
     return axis;
   };
@@ -129,6 +147,21 @@ d3.chart.axis = function() {
   return axis;
 };
 
+var d3_chart_axisEpsilon = 1e-10;
+
 function d3_chart_axisPath(range, size) {
   return "M" + range[0] + "," + size + "V0H" + range[1] + "V" + size;
+}
+
+function d3_chart_axisSubdivide(ticks) {
+  var ticks2 = [],
+      i = -1,
+      n = ticks.length,
+      d = (ticks[1] - ticks[0]) / 2;
+  ticks2.push(ticks[0] - d);
+  while (++i < n) {
+    ticks2.push(ticks[i]);
+    ticks2.push(ticks[i] + d);
+  }
+  return ticks2;
 }
